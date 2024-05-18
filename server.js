190d469e4a27ae5ac3b97fde894163063222103c
nuwol <90162269+nuwol@users.noreply.github.com>
2024-05-18 20:18:15 +0900

    Add files via upload

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// MongoDB 연결
+if (process.env.NODE_ENV !== 'production') {
+require('dotenv').config()
+}
+
+try {
+mongoose.connect(process.env.MONGO_DB_SECRET) //useNewUrlParser 옵션이 mongoose 5.3.0 이상 버전부터는 default 기능으로 내장되어 따로 옵션설정을 할 필요 없음.
+console.log('MongoDb 연결 완료')
+
+} catch (error) {
+console.log(error)
+}
+
+const homeRouter = require('./router/routes');
+
+
+// 라우터 설정
+app.use('/', homeRouter);
+
+
+app.listen(PORT, function(){
+console.log(`Listening on ${PORT}`);
+}); 
