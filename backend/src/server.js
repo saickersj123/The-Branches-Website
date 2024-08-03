305e6706f5201d485d7cc4874cb642c506164f4c
Hyeonseo <90162269+Hyeonseo1021@users.noreply.github.com>
2024-08-03 13:40:53 +0900

    Add files via upload

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,7 +10,7 @@ const memberRoutes = require('./routes/memberRoutes');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ credentials: true, origin: `http://localhost:${PORT}` }));
+app.use(cors({ credentials: true, origin: process.env.ALLOWED_ORIGIN }));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
